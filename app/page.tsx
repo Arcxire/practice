1f9a54536476aafe603e54f159a2eecee2c674ba
Arcxire <maxim.stepacov@iis.utm.md>
2024-09-07 21:41:24 +0300

    Rapid API Cars

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 import Image from "next/image";
 
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { fetchCars } from "@/utils";
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+
+  const isDataEmpty = !Array.isArray(allCars)  || allCars.length < 1 || !allCars;
+
   return (
     <main className="overflow_hidden">
       <Hero />
@@ -13,7 +18,8 @@ export default function Home() {
           <h1 className="text-4x1
           font-extrabold">Car Catalogue</h1>
           <p>Explore the cars you might like</p>
-          
+          </div>
+
           <div className="home__filters">
             <SearchBar />
 
@@ -23,7 +29,21 @@ export default function Home() {
               <CustomFilter title="year" />
             </div>
           </div>
-        </div>
+
+          {!isDataEmpty ? (
+            <section>
+              <div className="home__cars-wrapper">
+                {allCars?.map((car) => ( <CarCard car={car}/>
+                ))}
+              </div>
+            </section>
+          ): (
+            <div className="home__error-container">
+              <h2 className="text-black text-xl font-bold">Oops,no results</h2>
+              <p>{allCars}</p>
+            </div>
+          )}
+
       </div>
     </main>
   );
