0d07a0f402ab9b41a1ed361da0336a94894ed384
Arcxire <maxim.stepacov@iis.utm.md>
2024-09-12 20:09:18 +0300

    Show More

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from "@/components";
 import { fetchCars } from "@/utils";
 import { fuels, yearsOfProduction } from "@/constants";
 import { HomeProps } from "@/types";
@@ -44,6 +44,11 @@ export default async function Home({ searchParams }: HomeProps) {
                 {allCars?.map((car) => ( <CarCard car={car}/>
                 ))}
               </div>
+
+              <ShowMore 
+                pageNumber={(searchParams.limit || 10) / 10}
+                isNext={(searchParams.limit || 10) > allCars.length}
+              />
             </section>
           ): (
             <div className="home__error-container">
